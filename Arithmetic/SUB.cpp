ba59031b6ad6c92ff524ed0bca378735ff2f798f
ShaanManchanda <111204757+ShaanManchanda@users.noreply.github.com>
2023-08-19 20:26:57 +0530

    Add files via upload

---
+++ Arithmetic/SUB.cpp
@@ -0,0 +1,42 @@
+#include "../HeaderFiles/header.h"
+#include "../HeaderFiles/tools.h"
+#include "../HeaderFiles/arithematic.h"
+
+void SUB(string arg1,string &accumulator ,string registers[],bool flag[],map<string,string>&memory){
+	
+	int length=arg1.length();
+	if(length == 1){
+		if(validateRegister(arg1)||validateRegisterPair(arg1)){
+			if(arg1 != "M"){
+				/*Fetches index of register to access array string registers[]*/
+				int registerID = registerNumber(arg1);              
+				/*Converting decimal value to string format and storing in accumulator*/
+				accumulator = hexaSub(registers[registerID],accumulator,flag,true);
+			}
+			else{
+				/*Fetches data of HL pair*/
+				string address = "";
+				address = address + registers[4] + registers[5];
+				if(validateAddress(address)){
+					/*Converting decimal value to string format and storing in accumulator*/
+					accumulator = hexaSub(memory[address],accumulator,flag,true);				
+				}
+				else{
+					cout<<"Error: "<<arg1<<"\nAddress out of bounds\nThe program will quit\n";
+					exit(0);
+					/*error message of address out of bounds*/
+				}						
+			}
+		}
+		else{
+			cout<<"Error: "<<arg1<<"\nInvalid register details\nThe program will quit\n";
+			exit(0);
+			/*error of invalid register details*/
+		}
+	}
+	else{
+		cout<<"Error: "<<arg1<<"\nInvalid arguments\nThe program will quit\n";
+		exit(0);
+		/*Error message of invalid arguments*/
+	}
+}
